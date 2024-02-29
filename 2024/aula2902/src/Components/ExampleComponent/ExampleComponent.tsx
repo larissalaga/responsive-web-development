1be5e15a106af1de6fbfcdedbb5c6ea2a36735ab
Logon Aluno <logonrmlocal@fiap.com.br>
2024-02-29 12:04:54 -0300

    Aula 29/02/24-2

--- 2024/aula2902/src/Components/ExampleComponent/ExampleComponent.tsx
+++ 2024/aula2902/src/Components/ExampleComponent/ExampleComponent.tsx
@@ -2,17 +2,26 @@ import { useState } from "react";
 
 const ExampleComponent: React.FC = () => {
     const [text, setText] = useState("");
+    const [text2, setText2] = useState("");
 
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) =>{
         setText(event.target.value);
+        
     }
+    const handleChange2 = (event: React.ChangeEvent<HTMLInputElement>) =>{
+       
+        setText2(event.target.value);
+    }
+    
     return(
         <>
         <div>
             <b>Texto: </b>
-            <input type="text" onBlur={handleChange}/> {/*o onBlur modifica o conteudo somente quando sai da caixa de texto o onChande muda a todo momento o que foi modificado na caixa de texto*/}
+            <input type="text" onChange={handleChange}/> {/*o onBlur modifica o conteudo somente quando sai da caixa de texto o onChande muda a todo momento o que foi modificado na caixa de texto*/}
             <p>Texto digitado: {text} </p>
+            <input type="text" onBlur={handleChange2}/>
+            <p>Texto digitado: {text2} </p>
         </div>
 
         </>
