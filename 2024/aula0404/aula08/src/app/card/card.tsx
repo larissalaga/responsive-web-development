f59bd5a0e1a6f026aea65afa950f71e11aae5a13
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-04 12:10:36 -0300

    aula04/04

---
+++ 2024/aula0404/aula08/src/app/card/card.tsx
@@ -0,0 +1,42 @@
+"use client"
+import Link from "next/link";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+const Card = ()=>{
+    const[data, setData]=useState<any[]>([]);
+    useEffect(()=>{
+        const fetchData = async()=>{
+            const response = await fetch("/discos.json")
+            const jsonData = await response.json();
+            setData(jsonData);
+        }
+        fetchData();
+    },[])
+
+    const getRandomElements = (arr : any[], numElements: number) => {
+        const shuffled = arr.sort(() => 0.5 - Math.random());
+        return shuffled.slice(0,numElements);
+
+    }
+    const randomData = getRandomElements(data,8);
+
+    return(
+        <>
+            <div className="content">
+                <div className="content-card">
+                    {randomData.map((item) => (
+                        <div className="card" key={ item.id}>
+                            <Link href={"Produtos/" + item.slug}>
+                            <Image src={"/imagens/" + item.slug +".jpg"} alt="Teste" width={100} height={100} ></Image>
+                            </Link>
+                            <h2>{ item.nome }</h2>
+                        </div>
+                    ))}                
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Card;
